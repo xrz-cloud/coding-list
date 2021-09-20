d17349f8a6490080a49799bc0b6e5515cbcf1ab2
xrz-cloud <3545270592@qq.com>
2021-09-20 15:35:04 +0800

    2021.9.20

--- web/js/init.js
+++ web/js/init.js
@@ -14,6 +14,27 @@ function typeJSONorTXT(str) {
   console.log('It is not a string!')
 }
 
+function StringAs(string) {
+  //此函数用于将一个JSON字符串转义
+  //e.g. '{"a": "b"}' => '{\"a\":\"b\"}'
+  //来源：https://blog.csdn.net/zhangxin09/article/details/6582173
+  return '"' + string.replace(/(\\|\"|\n|\r|\t)/g, "\\$1") + '"';
+}
+
+function changeData(data, newKey, oldKey) {
+  let str = JSON.stringify(data);
+  let reg = new RegExp(oldKey, 'i');
+  let newStr = str.replace(reg, newKey);
+  return JSON.parse(newStr);
+}
+
+function tomlifyZ(data) {
+  let str = tomlify.toToml(data)
+  str = str.replace(new RegExp('"', 'g'), "'")
+  str = str.replace(new RegExp('\n', 'g'), "\\n")
+  return str
+}
+
 function LoadStep1() {
   window.location.hash = "#user-content-step1"
   TBC.innerHTML = `<h2 id="user-content-step1">第一步<small>新建子用户</small></h2>
@@ -102,7 +123,7 @@ async function LoadStep3() {
     if (ProjectList.length != 0) {
       let tbody = ""
       for (var i = 0; i < ProjectList.length; i++) {
-        tbody += `<tr onclick="window.location.href='/init?DisplayName=${ProjectList[i]["DisplayName"]}#user-content-step4'">
+        tbody += `<tr onclick="window.location.href='/init?Name=${ProjectList[i]["Name"]}#user-content-step4'">
           <td>${i+1}</td>
           <td>${ProjectList[i]["DisplayName"]}</td>
           <td>${ProjectList[i]["Name"]}</td>
@@ -189,9 +210,19 @@ function LoadStep4() {
 }
 
 async function InStep4(action = "GP", id = undefined) {
-  let coding_token = localStorage.getItem("coding_token") || document.getElementById("coding_token").value,
-    vercel_token = localStorage.getItem("vercel_token") || document.getElementById("vercel_token").value,
+  let coding_token, vercel_token, title
+  if (document.getElementById("coding_token") && document.getElementById("vercel_token") && document.getElementById("title")) {
+    coding_token = document.getElementById("coding_token").value
+    vercel_token = document.getElementById("vercel_token").value
+    title = document.getElementById("title").value
+    localStorage.setItem("coding_token", coding_token)
+    localStorage.setItem("vercel_token", vercel_token)
+    localStorage.setItem("title", title)
+  } else {
+    coding_token = localStorage.getItem("coding_token") || document.getElementById("coding_token").value
+    vercel_token = localStorage.getItem("vercel_token") || document.getElementById("vercel_token").value
     title = localStorage.getItem("title") || document.getElementById("title").value
+  }
   document.getElementById("TBCS").innerHTML = `<div class="mdui-progress">
   <div class="mdui-progress-indeterminate"></div>
   </div>`
@@ -264,18 +295,45 @@ async function InStep4(action = "GP", id = undefined) {
         }
       })
       CPEV_coding_token = await CPEV_coding_token.json()
-      if (CPEV_title.id && CPEV_coding_token.id) LoadStep5()
+      if (CPEV_title.id && CPEV_coding_token.id) {
+        document.getElementById("TBCS").innerHTML=`现在，请务必打开<a href="https://vercel.com/dashboard">Vercel</a>，找到部署此网站的项目，`
+        LoadStep5()
+      }
     }
   }
   return false
 }
 
-async function LoadStep5() {
+function LoadStep5() {
   window.location.hash = "#user-content-step5"
   TBC.innerHTML = `<h2 id="user-content-step5">第五步<small>创建首页</small></h2>
-    <span id="TBCS"><div class="mdui-progress">
-    <div class="mdui-progress-indeterminate"></div>
-    </div></span>
+  <div class="mdui-textfield mdui-textfield-floating-label">
+    <label class="mdui-textfield-label">顶部消息/公告</label>
+    <input id="des_top" class="mdui-textfield-input"/>
+  </div>
+  <div class="mdui-divider"></div>首页文件夹显示
+  <div class="mdui-table-fluid"><table class="mdui-table mdui-table-hoverable">
+    <thead><tr>
+      <th>#</th>
+      <th mdui-tooltip="{'content': '即IssueCode，应为除了0之外的数字，否则保存时会被清空。'}">指向事项Code</th>
+      <th mdui-tooltip="{'content': '即文件夹名称，用于表明文件夹/事项的作用。'}">列表显示名称</th>
+      <th>操作</th>
+    </tr></thead>
+    <tbody id="TBCS"></tbody>
+  </table></div>
+  <button class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple" onclick="InStep5_add()">新增</button>
+  <button class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple" onclick="InStep5_clear()">清空</button><br><br>
+  <div class="mdui-divider"></div>
+  <div class="mdui-textfield mdui-textfield-floating-label">
+    <label class="mdui-textfield-label">底部消息</label>
+    <input id="des_bottom" class="mdui-textfield-input"/>
+  </div>
+  <button class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple" onclick="InStep5_done()">保存并继续</button>
+    <h3>帮助</h3>
+    如何查找Code(IssueCode)：查看下图，图上的<code>IssueCode</code>是<code>82</code>。<br>
+    <img src="/docs/img/8.png" alt="8.png" class="mdui-img-fluid mdui-img-rounded" width="50%" height="50%"><br>
+    各种值的含义：<br>
+    <img src="/docs/img/10.png" alt="10.png" class="mdui-img-fluid mdui-img-rounded" width="50%" height="50%"><br>
     <div class="mdui-bottom-nav mdui-bottom-nav-scroll-hide">
       <div class="mdui-row-xs-2">
         <div class="mdui-col">
@@ -287,9 +345,111 @@ async function LoadStep5() {
         </div>
       </div>
     </div>`
-  document.getElementById("TBCS").innerHTML = `<form name="input" action="#user-content-step5" method="get">
-  <input type="submit" value="确认" class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple">
-  </form>`
+  let desktop = JSON.parse(localStorage.getItem("desktop")) || []
+  localStorage.setItem("desktop", JSON.stringify(desktop))
+  InStep5_gen()
+}
+
+function InStep5_gen() {
+  let desktop = JSON.parse(localStorage.getItem("desktop")) || [],
+    list = ""
+  for (let i = 0; i < desktop.length; i++) {
+    list += `<tr>
+    <td>${i+1}</td>
+    <td><input class="mdui-textfield-input" type="text" id="dt-code-${i}" value="${desktop[i].Code || ""}" placeholder="点击我即可修改Code,为除0以外的数字"/></td>
+    <td><input class="mdui-textfield-input" type="text" id="dt-name-${i}" value="${desktop[i].Name || ""}" placeholder="点击我即可修改Name"/></td>
+    <td><button class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple" onclick="InStep5_remove(${i})">删除</button></td></tr>`
+  }
+  document.getElementById("TBCS").innerHTML = list
+}
+
+function InStep5_save() {
+  let desktop = JSON.parse(localStorage.getItem("desktop")) || []
+  for (let i = 0; i < desktop.length; i++) {
+    desktop[i]["Code"] = Number(document.getElementById(`dt-code-${i}`).value)
+    desktop[i]["Name"] = document.getElementById(`dt-name-${i}`).value
+    desktop[i]["Type"] = "SUB_TASK"
+  }
+  localStorage.setItem("desktop", JSON.stringify(desktop))
+}
+
+function InStep5_add() {
+  InStep5_save()
+  let desktop = JSON.parse(localStorage.getItem("desktop")) || []
+  desktop.push({})
+  localStorage.setItem("desktop", JSON.stringify(desktop))
+  InStep5_gen()
+}
+
+function InStep5_remove(ele) {
+  let desktop = JSON.parse(localStorage.getItem("desktop")) || []
+  desktop.splice(ele, 1)
+  localStorage.setItem("desktop", JSON.stringify(desktop))
+  InStep5_save()
+  InStep5_gen()
+}
+
+function InStep5_clear() {
+  localStorage.removeItem("desktop")
+  location.reload()
+}
+
+async function InStep5_done() {
+  InStep5_save()
+  let desktop = JSON.parse(localStorage.getItem("desktop")) || [],
+    des_top = document.getElementById("des_top").value,
+    des_bottom = document.getElementById("des_bottom").value,
+    index = {
+      "status": 200,
+      "RequestId": "null",
+      "msg": {
+        "Type": "MISSION",
+        "Name": "INDEX",
+        "Description": {
+          "textarea": {
+            "top": des_top,
+            "bottom": des_bottom
+          }
+        },
+        "Files": [],
+        "SubTasks": desktop,
+        "Parent": 404
+      }
+    },
+    ClistConfig = encodeURIComponent(tomlifyZ({
+      "index": index,
+      "title": localStorage.getItem("title"),
+      "refresh": `${1*60*60*1000}`,
+      "share": {
+        "github_repo": "https://github.com/xrz-cloud/coding-list"
+      }
+    })) //重要提示：此处Description直接以json转toml储存，故获取信息后需手动转换为字符串形式json
+  let MW = await fetch(`/api/PAPIS?body={"Action": "ModifyWiki","ProjectName": "${new URLSearchParams(location.search).get("Name")}","Title": "ClistConfig","Content": "${ClistConfig}","Iid": 2,"Msg": "创建了首页、标题、刷新时间。可以按照格式修改，参照coding-list项目文档。"}&token_type=${oauth.token_type}&token=${oauth.access_token}`)
+  MW = await MW.json()
+  MW = MW.msg.Response
+  if (!MW.Error && MW.Data) {
+    LoadStep6()
+  } else {
+    document.getElementById("TBCS").innerHTML = `好像出了些奇怪的问题呢？<br>
+    按下<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>I</kbd>或<kbd>Fn</kbd>+<kbd>F12</kbd>打开 开发者工具=>控制台 查看日志<br>
+    推测是token失效。`
+  }
+}
+
+function LoadStep6() {
+  window.location.hash = "#user-content-step6"
+  TBC.innerHTML = `<h2 id="user-content-step6">第六步<small>完成&帮助</small></h2>
+  <p>恭喜，到目前为止，所有基础配置已完成，本程序已经可以使用。<a href="/">点我前往首页</a><br>
+  你可以在CodingList项目中找到WIKI的#2，并按照帮助对其进行配置的修改。(我们使用 TOML v4.0 作为配置文件，<a href="https://toml.io/cn/v0.4.0" target="_blank">语法参考</a>)<br>
+  也可以在本程序的管理界面进行图形化管理。</p>
+    <div class="mdui-bottom-nav mdui-bottom-nav-scroll-hide">
+      <div class="mdui-row-xs-2">
+        <div class="mdui-col">
+          <button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-btn-raised"
+            onclick="LoadStep5()">上一步</button>
+          </div>
+      </div>
+    </div>`
 }
 
 if (window.location.hash == "") window.location.hash = "#user-content-step1"
@@ -354,8 +514,10 @@ if (window.location.hash == "#user-content-step2") {
         localStorage.setItem("team", oauth.team)
       } else {
         console.warn("API测试返回数据", DCCU)
+        sessionStorage.removeItem("oauth")
         document.getElementById("TBCS").innerHTML = `好像出了些奇怪的问题呢？<br>
-        按下<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>I</kbd>或<kbd>Fn</kbd>+<kbd>F12</kbd>打开 开发者工具=>控制台 查看日志`
+        按下<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>I</kbd>或<kbd>Fn</kbd>+<kbd>F12</kbd>打开 开发者工具=>控制台 查看日志<br>
+        推测是token失效，以为您清除保存的token信息，请刷新本页面重试。`
       }
     }
     DCCU()
@@ -375,56 +537,64 @@ if (window.location.hash == "#user-content-step3" && !!oauth) {
         例如：封面。<br>
         <strong>提示：</strong><ins>如果需要重新配置程序，请从头开始初始化。不在此初始化可能导致程序出错。</ins><br>
         <div class="mdui-divider"></div>
-        在点击按钮前，请先打开<a href="https://${localStorage.getItem('team')}.coding.net/p/${new URLSearchParams(location.search).get('Name')}/pm-initialize" target="_blank">此链接</a>，并按照图片初始化。<br>
+        在点击按钮前，请先打开<a href="https://${localStorage.getItem('team')}.coding.net/p/${new URLSearchParams(location.search).get('Name')}/pm-initialize" target="_blank" class="mdui-btn mdui-color-theme-accent mdui-ripple mdui-btn-raised">此链接</a>，并按照图片初始化。<br>
         <img src="/docs/img/9.png" alt="9.png" class="mdui-img-fluid mdui-img-rounded" width="50%" height="50%"><br>
-        <a href="/init?action=CIinit&ProjectName=${new URLSearchParams(location.search).get("Name")}#user-content-step3" class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-btn-raised">开始程序配置</a></p>`
-      } else {
-        document.getElementById("TBCS").innerHTML = `好像出了些奇怪的问题呢？<br>
-        按下<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>I</kbd>或<kbd>Fn</kbd>+<kbd>F12</kbd>打开 开发者工具=>控制台 查看日志<br>`
+        <a href="/init?action=CIinit&Name=${new URLSearchParams(location.search).get("Name")}#user-content-step3" class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-btn-raised">开始程序配置</a></p>`
       }
     }
     CCP()
-  } else if (new URLSearchParams(location.search).get("action") == "CIinit" && new URLSearchParams(location.search).get("ProjectName")) {
+  } else if (new URLSearchParams(location.search).get("action") == "CIinit" && new URLSearchParams(location.search).get("Name")) {
     async function CIinit() {
-      let CIinit = await fetch(`/api/PAPIS?body={"Action": "CreateIssue","ProjectName": "${new URLSearchParams(location.search).get("ProjectName")}","Type": "MISSION","Name": "ClistConfig","Priority": "3","Description": "{'clist':{}}"}&token_type=${oauth.token_type}&token=${oauth.access_token}`)
+      let CIinit = await fetch(`/api/PAPIS?body={"Action": "CreateIssue","ProjectName": "${new URLSearchParams(location.search).get("Name")}","Type": "MISSION","Name": "ClistConfig","Priority": "3","Description": "修改配置请查看WIKI的%232"}&token_type=${oauth.token_type}&token=${oauth.access_token}`)
       CIinit = await CIinit.json()
       CIinit = CIinit.msg.Response
       if (!CIinit.Error && CIinit.Issue) {
-        TBC.innerHTML = `<h2 id="user-content-step3">第三步<small>选择存储数据的项目</small></h2>
-          <span id="TBCS"><div class="mdui-progress">
-          <div class="mdui-progress-indeterminate"></div>
-          </div></span>
-          <div class="mdui-bottom-nav mdui-bottom-nav-scroll-hide">
-            <div class="mdui-row-xs-2">
+        let CW = await fetch(`/api/PAPIS?body={"Action": "CreateWiki","ProjectName": "${new URLSearchParams(location.search).get("Name")}","Title": "ClistConfig","Content": "${tomlifyZ({'test': '测试'})}","ParentIid": 0,"Msg": "初始化"}&token_type=${oauth.token_type}&token=${oauth.access_token}`)
+        CW = await CW.json()
+        CW = CW.msg.Response
+        if (!CW.Error && CW.Data) {
+          TBC.innerHTML = `<h2 id="user-content-step3">第三步<small>选择存储数据的项目</small></h2>
+            <span id="TBCS"><div class="mdui-progress">
+            <div class="mdui-progress-indeterminate"></div>
+            </div></span>
+            <div class="mdui-bottom-nav mdui-bottom-nav-scroll-hide">
+              <div class="mdui-row-xs-2">
+                <div class="mdui-col">
+                  <button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-btn-raised"
+                    onclick="LoadStep2()">上一步</button>
+              </div>
               <div class="mdui-col">
-                <button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-btn-raised"
-                  onclick="LoadStep2()">上一步</button>
+                  <button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-btn-raised"
+                    onclick="window.location.href='/init?Name=${new URLSearchParams(location.search).get("Name")}#user-content-step4'">下一步</button>
+              </div>
             </div>
-            <div class="mdui-col">
-                <button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-btn-raised"
-                  onclick="window.location.href='/init?DisplayName=${ProjectList[i]["DisplayName"]}#user-content-step4'">下一步</button>
-            </div>
-          </div>
-          </div>`
-        document.getElementById("TBCS").innerHTML = `<p>我们在你新建的项目中创建了一个叫<code>ClistConfig</code>事项。<br>
-        请点击下一步以继续。</p>`
+            </div>`
+          document.getElementById("TBCS").innerHTML = `<p>我们在你新建的项目中创建了一个叫<code>ClistConfig</code>事项。(此事项应为#1，用于标记此项目为clist)<br>
+          并且，我们在你的项目中创建了为#2的WIKI，这个WIKI用来存放配置文件。<br>
+          请确认<a href="https://${localStorage.getItem('team')}.coding.net/p/${new URLSearchParams(location.search).get('Name')}/wiki/2" target="_blank">此链接</a>中，显示了以下文字：<br>
+          test = '测试'<br>
+          请点击下一步以继续。</p>`
+        } else {
+          document.getElementById("TBCS").innerHTML = `好像出了些奇怪的问题呢？<br>
+          按下<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>I</kbd>或<kbd>Fn</kbd>+<kbd>F12</kbd>打开 开发者工具=>控制台 查看日志`
+        }
       } else {
         document.getElementById("TBCS").innerHTML = `好像出了些奇怪的问题呢？<br>
         按下<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>I</kbd>或<kbd>Fn</kbd>+<kbd>F12</kbd>打开 开发者工具=>控制台 查看日志
         <div class="mdui-divider"></div>
         您可能没有在点击按钮前进行初始化，请先打开<a href="https://${localStorage.getItem('team')}.coding.net/p/${new URLSearchParams(location.search).get('ProjectName')}/pm-initialize" target="_blank">此链接</a>，并按照图片初始化。<br>
         <img src="/docs/img/9.png" alt="9.png" class="mdui-img-fluid mdui-img-rounded" width="50%" height="50%"><br>
-        <a onclick="reload()" class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-btn-raised">重新开始程序配置(刷新本页)</a>`
+        <a onclick="location.reload()" class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple mdui-btn-raised">重新开始程序配置(刷新本页)</a>`
       }
     }
     CIinit()
   }
-} else LoadStep2
+}
 
 if (window.location.hash == "#user-content-step4" && !!oauth) LoadStep4()
-else LoadStep2
 
 if (window.location.hash == "#user-content-step5" && !!oauth) LoadStep5()
-else LoadStep2
+
+if (window.location.hash == "#user-content-step6") LoadStep6()
 
 mdui.mutation()
